aa002c4057efc20293c80d174da8296c5a4b44c4
PowieT <PowieT@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-01-20 11:22:25 +0000

    0.9.5 - CSS for a clean link list on all themes
    git-svn-id: https://plugins.svn.wordpress.org/plinks/tags/0.9.5@655691 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ plinks_settings.php
@@ -0,0 +1,81 @@
+<div class="wrap">
+<div class="icon32" id="icon-options-general"></div>
+<h2><?php _e('pLinks Settings', 'plinks') ?></h2>
+<form method="post" action="options.php">
+<?php
+settings_fields( 'plinks-settings' );
+?>
+<div id="poststuff">
+<div class="postbox">
+<h3><?php _e('Thumbnail Settings', 'plinks') ?></h3>
+	<div class="inside">
+    <table class="form-table">
+        <tr valign="top">
+        <th scope="row"><?php _e('Show Preview Images', 'plinks') ?></th>
+        <td><input type="checkbox" name="websnapr-show" id="websnapr-show" value="1" <?php checked(get_option('websnapr-show'), 1); ?> /></td>
+        </tr>
+
+        <tr valign="top">
+        <th scope="row"><?php _e('Thumbnail Image Size', 'plinks') ?></th>
+        <td>
+        	<select name="websnapr-size">
+        		<option value="t" <?php if (get_option('websnapr-size')=='t') print('selected'); ?>><?php _e('tiny', 'plinks') ?> - 90x68px</option>
+        		<option value="s" <?php if (get_option('websnapr-size')=='s') print('selected'); ?>><?php _e('small', 'plinks') ?> - 120x90px</option>
+        		<option value="m" <?php if (get_option('websnapr-size')=='m') print('selected'); ?>><?php _e('medium', 'plinks') ?> - 200x150px</option>
+        		<option value="l" <?php if (get_option('websnapr-size')=='l') print('selected'); ?>><?php _e('large', 'plinks') ?> - 400x300px</option>
+        		<option value="x" <?php if (get_option('websnapr-size')=='x') print('selected'); ?>><?php _e('extra large', 'plinks') ?> - 480x380px</option>
+        	</select>
+        </td>
+        </tr>
+    </table>
+    </div>
+</div>
+</div>
+<input type="submit" class="button-primary" value="<?php _e('Save Changes') ?>" />
+</form>
+<br />
+
+<div id="poststuff">
+<div class="postbox">
+<h3><?php _e('pLinks Usage', 'plinks') ?></h3>
+	<div class="inside">
+	<?php _e('Simple use a shortcode on a page of your choice or create a new page and insert the following shortcode: <code>[plinks category_name="Blogroll"]</code>. Use a category that exists!', 'plinks') ?>
+	<br /><br />
+	<?php _e('To display a simple PagePeeker preview: <code>[pagepeeker url=http://www.powie.de]</code>','plinks') ?>
+	<br /><br />
+	<?php _e('To change the size of the preview image use the <b>size</b> parameter: <code>[pagepeeker url=http://www.powie.de size=l]</code>','plinks') ?>
+	<br />
+	<?php _e('Size is defined by PagePeeker: <code>t</code>, <code>s</code>, <code>m</code>, <code>l</code>, <code>x</code>','plinks') ?>
+	<br />
+	<?php _e('If no size is defined it uses the settings above!','plinks') ?>
+	<br /><br />
+	<?php _e('Happy Linking.', 'plinks') ?>
+    </div>
+</div>
+
+<div class="postbox">
+<h3><?php _e('About', 'plinks') ?></h3>
+	<div class="inside" style="overflow:auto">
+		<div style="float:left;margin-right: 10px; display:inline;">
+		<!-- www -->
+		WWW: <a href="http://www.powie.de">powie.de</a>
+		</div>
+		<div style="float:left;margin-right: 10px; display:inline;">
+		<!-- twitter -->
+		<a href="https://twitter.com/PowieT" class="twitter-follow-button" data-show-count="false" data-lang="de">@PowieT folgen</a>
+		<script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0];if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src="//platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");</script>
+		</div>
+		<div style="float:left;margin-right: 10px; display:inline;">
+		<!-- fb -->
+		<iframe src="//www.facebook.com/plugins/like.php?href=https%3A%2F%2Fwww.facebook.com%2Fpowiede&amp;send=false&amp;layout=standard&amp;width=450&amp;show_faces=false&amp;font&amp;colorscheme=light&amp;action=like&amp;height=35" scrolling="no" frameborder="0" style="border:none; overflow:hidden; width:350px; height:35px;" allowTransparency="true"></iframe>
+		</div>
+		<div style="float:left;margin-right: 10px; display:inline;">
+		<!-- PagePeeker -->
+		<a href="http://www.pagepeeker.com">PagePeeker</a> Website Preview Images
+		</div>
+    </div>
+</div>
+
+</div>
+
+</div>
---
+++ readme.txt
@@ -0,0 +1,71 @@
+=== Powie's pLinks ===
+Contributors: PowieT
+Donate link: https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&hosted_button_id=UST438WYW6HNJ
+Tags: links, blogroll, plugin, pagepeeker, shortcode
+Requires at least: 2.8
+Tested up to: 3.5
+Stable tag: 0.9.5
+License: GPLv2
+
+Displays a linklist at a page with the [plinks] shortcode with PagePeeker preview images. Provides a [pagepeeker] shortcode
+
+== Description ==
+This Plugin shows a List from a Link Category on a Page using the [plinks] shortcode. You can provide the category as a parameter to choose which list is displayed.
+To view a single website preview image, provided by [PagePeeker](http://www.pagepeeker.com) you can use the [pagepeeker] shortcode.
+
+= Including: =
+* PagePeeker Preview Images 
+* Shortcode: [pagepeeker]
+* Link list from links manager
+* Shortcode [plinks]
+
+= Support =
+Support Forum @ [powie.de](http://goo.gl/lfR7B)
+
+== Installation ==
+1. Upload this Plugin to the `/wp-content/plugins/` directory
+2. Activate the plugin through the 'Plugins' menu in WordPress
+3. Adjust settings
+
+== Remove plugin ==
+1. Deactivate plugin through the 'Plugins' menu in WordPress
+2. Delete plugin through the 'Plugins' menu in WordPress
+
+== Screenshots ==
+1. Usage on a page
+
+== Frequently Asked Questions ==
+nothing now
+
+= How can I support you? =
+Post a comment on [powie.de Forum](http://forum.powie.de))
+
+= What is the plugin page?  =
+[Powie's Flying Dev Blog](http://www.powie.de)
+
+= Where do I post my feedback? =
+Post it at my Forum: [powie.de Forum](http://forum.powie.de)
+
+== Changelog ==
+= 0.9.5 (20.01.2013) =
+* Added a plinks.css style to have a clean list in all themes
+
+= 0.9.4 (15.01.2013) =
+* New PagePeeker V2 API used
+
+= 0.9.3 (07.02.2013) =
+* Localization ready
+* de_DE Language
+* new pagepeeker shortcode
+
+= 0.9.2 (04.11.2011) =
+* Icon Fix
+
+= 0.9.1 (04.11.2011) =
+* Websnapr removed -> PagePeeker used
+
+= 0.9.0 (19.02.2011) =
+* first version with all required stuff by me
+
+== Upgrade Notice ==
+Simple install new version
